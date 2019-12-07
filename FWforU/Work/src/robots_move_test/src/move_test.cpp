ab4077579568e5d31981d9239d9f2a81d854614f
Wenjun Xin <1390201584@qq.com>
2019-12-07 08:32:15 +0800

    对避障进行调研，添加论文

---
+++ FWforU/Work/src/robots_move_test/src/move_test.cpp
@@ -0,0 +1,20 @@
+#ifndef VELCONTROL_H
+#define VELCONTROL_H
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#include <nav_msgs/Odometry.h>
+#include "tf/LinearMath/Matrix3x3.h"
+#include "geometry_msgs/Quaternion.h"
+#define CV_PI 3.1415926535897932384626433832795
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+#endif // !VELCONTROL_H
